36d70a2cb3b7bb69325605766bed605ac952ca32
Brad Volts <Brad@Brads-MacBook-Pro-2.local>
2016-09-13 22:26:31 -0700

    Get a token to place
    Several attempts to get token in the correct row.
    Getting stuck.

--- README.md
+++ README.md
@@ -11,7 +11,9 @@ Declare nav variable.
 Create loop to add 7 buttons.
 Declare variable for buttons and create a button.
 Add class to buttons.
+Add id to buttons to give them individual names.
 Append the button to the nav.
+Create event listener for buttons.
 
 Columns:
 Declare container variable.
@@ -29,6 +31,8 @@ Append the token to the column.
 42 seperate divs? Use for loops
 rows and columns. nested for loops.
 Use ids for individual, class for groups, ie. rows, columns.
+
+9/12/16=>
 In css:
 Make a class that will a div height and width and background-color
 
@@ -42,3 +46,25 @@ Setting each column div to have a display of inline-block
 
 Reach:
 adding a nested for loop that will create rows
+
+9/13/16=>
+Write out reverse pseudo code for the column and token loops
+
+Add in an event listener and handler for the tokens
+
+Investigate and pseudo code whether the listener should be on a column or a token
+
+Somehow put buttons at the top of each column
+
+Getting a token to place
+
+Getting it into the right column
+
+Getting it into the right row
+
+button should have an id of A,B,C
+when you click a button the id should be saved as a variable,
+and all elements with class name of the button id variable  should be grabbed in an array
+getElementsByClassName('className') returns array
+start at the bottom, and work your way up until you find
+a blue circle, then change it to red
--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-window.onload = function(){
+window.onload = function() {
   console.log('heeloooo');
   // declare nav variable
   var nav = document.getElementById('nav');
@@ -10,29 +10,52 @@ window.onload = function(){
     buttons.id = "b" + x;
     nav.appendChild(buttons);
 
-    buttons.addEventListener("click", function() {
-      console.log('Hello World');
-    });
+
+
+    var red = function() {
+        token.id = "red";
+      }
+  // var A = function() {
+  //   this.ClassName = "token A";
+  // }
+//button should have an id of A,B,C
+//when you click a button the id should be saved as a variable,
+//and all elements with class name of the button id variable  should be grabbed in an array
+// getElementsByClassName('className') returns array
+// start at the bottom, and work your way up until you find
+// a blue circle, then change it to red
+
+    // };
+    buttons.addEventListener("click", red);
   };
+
+  //grab all the buttons
+  //add event listener that reads the id, then calls a function
+
+
+      // buttons.addEventListener("click", function(){
+      //   this.getElementById("b1");
+      //   var b1 = function() {
+      //
+      //   }
+      //   change color(id)
+      // });
+
   // declare container variable
   var container = document.getElementById('container');
   console.log(container);
   // create loop to add 7 columns
-  for (var i = 0; i < 7; i++) {
+  for (var i = 1; i <= 7; i++) {
     var column = document.createElement('div');
     column.className = "column";
     container.appendChild(column);
   // create loop to add 6 tokens inside columns
-    for (var j = 0; j < 6; j++) {
+    for (var j = 1; j <= 6; j++) {
       var token = document.createElement('div');
       token.className = "token";
       column.appendChild(token);
     };
   };
-  // var piece = [];
-  // var addToken = function() {
-  //   piece[i]++;
-  // };
 
-
+// close window.onload
 };
