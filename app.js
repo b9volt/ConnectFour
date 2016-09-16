bec82c91c141684b4643fda796eee658172234da
Brad Volts <Brad@Brads-MacBook-Pro-2.local>
2016-09-16 11:43:35 -0700

    Get pieces alternating colors
    Pieces are alternating colors,
    beginning to work on winning conditions.

--- app.js
+++ app.js
@@ -3,24 +3,60 @@ window.onload = function() {
   // declare nav variable
   var nav = document.getElementById('nav');
   console.log(nav);
+  // declare player variable.
+  var player = 1;
   // function for buttonClick to be called when user clicks a button
+
   var buttonClick = function() {
       // token.classList.add("red");
       // console.log("red");
       console.log("This is button id " + this.id);
       // this.id calls on the button that is pressed
-      console.log(this.id.charAt(1));
+      // console.log(this.id.charAt(1));
       // charAt(1) calls the index of the button
       // By adding the id of 'c', it also references its coresponding column
       var column = document.getElementById('c' + this.id.charAt(1));
-      // console.log('These are the columns children: ', column.children);
+      console.log('These are the columns children: ', column.children);
       // console.log('This is the last child of the column: ', column.children[column.children.length - 1]);
-      var lastChild = column.children[column.children.length - 1];
-      console.log(lastChild);
-      lastChild.classList.add('red');
-      // var s = document.getElementsByClassName(column).children;
-      // s[1].classList.add("red");
+      // var lastChild = column.children[column.children.length - 1];
+      // console.log(lastChild);
+      // lastChild.classList.add('red');
+      // Loop backwards by accessing last child with column.length - 1.
+      for (var p = column.children.length -1; p >= 0; p--){
+      console.log(p);
+      if (column.children[p].classList.contains('red') || column.children[p].classList.contains('black')) {
+        continue;
+      } else {
+        if (player == 1) {
+          column.children[p].classList.add('red');
+          player = 0;
+        } else {
+          column.children[p].classList.add('black');
+          player = 1;
+        }
+        // column.children[p].classList.add('red');
+        break;
+      }
+    }
+
+        console.log(player);
+      win();
     };
+
+    var win = function() {
+      var column1 = document.getElementById('c1');
+      console.log("I am Column 1!")
+      for (r = 0; c1)
+        console.log("win function running")
+      if (c1 == 1 > 3) {
+        console.log("Red Wins!")
+      } else if (player == 0 > 3) {
+        console.log("Black Wins!")
+      } else {
+        player = 1;
+      }
+    }
+
   // create loop to add 7 buttons
   for (var x = 1; x <= 7; x++) {
     var buttons = document.createElement('button');
